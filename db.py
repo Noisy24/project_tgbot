77a289cb1f91ee17fab0fb8198619c39623deb78
Noisy <ak.leonov24@gmail.com>
2023-04-22 16:31:21 +0500

    добавление файлов

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 def start():
     conn = sqlite3.connect('main.db')
     cur = conn.cursor()
@@ -8,9 +9,9 @@ def start():
     company_name TEXT,
     model_name TEXT,
     price INTEGER);''')
-    cur.execute('''CREATE TABLE IF NOT EXISTS users(
-    user_id INTEGER
-    balance INTEGER
+    cur.execute('''CREATE TABLE users(
+    user_id INTEGER,
+    balance INTEGER,
     basket TEXT);''')
     cur.execute('''CREATE TABLE IF NOT EXISTS payments(
     bill_id TEXT,
@@ -23,7 +24,7 @@ def start():
 def add_product(company_name, model_name, price):
     conn = sqlite3.connect('main.db')
     cur = conn.cursor()
-    cur.execute(f'''INSERT INTO products(company_name, model_name, price) VALUES ('{company_name}', '{model_name}', '{price});''')
+    cur.execute(f'''INSERT INTO products(company_name, model_name, price) VALUES ('{company_name}', '{model_name}', '{price}');''')
     conn.commit()
 
 
@@ -44,5 +45,78 @@ def create_user(user_id):
     cur.execute(f'''INSERT INTO users(user_id, balance, basket) VALUES ({user_id}, 0, '');''')
     conn.commit()
 
-def get_company_names(company_name):
-    pass
+
+def get_company_names():
+    conn = sqlite3.connect('main.db')
+    cur = conn.cursor()
+    cur.execute("SELECT DISTINCT company_name FROM products")
+    l = set()
+    data = cur.fetchall()
+    for row in data:
+        for x in row:
+            l.add(x)
+    return l
+
+
+def get_company_products(company_name):
+    conn = sqlite3.connect('main.db')
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM products WHERE company_name = '{company_name}'")
+    data = cur.fetchall()
+    return data
+
+
+def clean_basket(user_id):
+    conn = sqlite3.connect('main.db')
+    cur = conn.cursor()
+    cur.execute(f'''UPDATE users SET basket = '' WHERE user_id = {user_id}''')
+    conn.commit()
+
+
+
+def create_basket_txt(user_id):
+    conn = sqlite3.connect('main.db')
+    cur = conn.cursor()
+    cur.execute(f"SELECT basket FROM users WHERE user_id = {user_id};")
+    basket = cur.fetchone()[0]
+    with open(f"{user_id}_basket.txt", "w") as f:
+        f.write(basket)
+
+
+def get_user(user_id):
+    conn = sqlite3.connect('main.db')
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM users WHERE user_id = {user_id};")
+    data = cur.fetchone()
+    return data
+
+
+def add_product_to_basket(user_id, product_id):
+    conn = sqlite3.connect('main.db')
+    cur = conn.cursor()
+    cur.execute(f"SELECT basket FROM users WHERE user_id = {user_id};")
+    basket = cur.fetchone()[0]
+    product = get_product_by_id(product_id)
+    if product:
+        if basket:
+            basket += f",{product_id}"
+        else:
+            basket = str(product_id)
+        cur.execute(f"UPDATE users SET basket = {basket} WHERE user_id = {user_id};")
+        conn.commit()
+        return True
+    return False
+
+
+def spend_balance(user_id, cost):
+    conn = sqlite3.connect('main.db')
+    cur = conn.cursor()
+    cur.execute(f"SELECT balance FROM users WHERE user_id = {user_id};")
+    balance = cur.fetchone()[0]
+    if balance >= cost:
+        new_balance = balance - cost
+        cur.execute(f"UPDATE users SET balance = {new_balance} WHERE user_id = {user_id};")
+        conn.commit()
+        return True
+    return False
+
--- main.py
+++ main.py
@@ -1,38 +1,138 @@
-import telebot
-from telebot import types
+import logging
 import db
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+import config
+
+API_TOKEN = config.telegram_token
+logging.basicConfig(level=logging.INFO)
+storage = MemoryStorage()
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot, storage=storage)
 
 
-bot = telebot.TeleBot('5923980996:AAF_jL5eHv0gIHC98-_p2LTP-wQ8gzXTPrA')
+class States(StatesGroup):
+    get_amount = State()
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
     markup = types.InlineKeyboardMarkup()
-    btn1 = types.InlineKeyboardButton("Товары", callback_data='show_products')
-    btn2 = types.InlineKeyboardButton("Корзина", callback_data='show_basket')
-    btn3 = types.InlineKeyboardButton("Пополнить баланс", callback_data='get_money')
-    markup.row(btn1, btn2, btn3)
-    bot.reply_to(message, 'Главное меню', reply_markup=markup)
+    btn1 = types.InlineKeyboardButton(text='Товары', callback_data='show_products')
+    btn2 = types.InlineKeyboardButton(text='Корзина', callback_data='show_basket')
+    btn3 = types.InlineKeyboardButton(text='Пополнить баланс', callback_data='balance')
+    btn4 = types.InlineKeyboardButton(text='Поддержка', url=config.admin_link)
+    markup.add(btn1, btn2, btn3, btn4)
+    await message.answer('Главное меню', reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda callback: True)
-def show_company_products(callback):
+@dp.callback_query_handler(lambda query: query.data == 'show_products')
+async def show_products(callback_query: types.CallbackQuery):
+    company_names = db.get_company_names()
     markup = types.InlineKeyboardMarkup()
     for company in company_names:
         markup.add(types.InlineKeyboardButton(text=company, callback_data=f'company_{company}'))
-    callback_query.message.answer('Выберите производителя', reply_markup=markup)
+    await callback_query.message.answer('Выберите производителя', reply_markup=markup)
 
 
-@dp.callback_query_handler(lambda query: query.startswith('company_'))
-async def show_company_product(callback_query: types.CallbackQuery):
+@dp.callback_query_handler(lambda query: query.data.startswith('company_'))
+async def show_company_products(callback_query: types.CallbackQuery):
     company = callback_query.data.split('_')[1]
     markup = types.InlineKeyboardMarkup()
     products = db.get_company_products(company)
+    print(db.get_company_products(company))
     for product in products:
-        markup.add(types.InlineKeyboardButton(text=product[1], callback_data=f'show_id_{product[0]}'))
+        markup.add(types.InlineKeyboardButton(text=product[2], callback_data=f'show_id_{product[0]}'))
     markup.add(types.InlineKeyboardButton(text='Назад', callback_data='show_products'))
+    await callback_query.message.edit_text(text='Выберите товар', reply_markup=markup)
 
 
+@dp.callback_query_handler(lambda query: query.data.startswith('show_id_'))
+async def show_product(callback_query: types.CallbackQuery):
+    product_id = callback_query.data.split('_')[2]
+    product = db.get_product_by_id(int(product_id))
+    product_id, company_name, model_name, price = product
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton(text='Добавить в корзину', callback_data=f'add_basket_{product_id}'))
+    markup.add(types.InlineKeyboardButton(text='Вернуться назад', callback_data=f'company_{company_name}'))
+    text = f'''Название модели: {model_name}
+Производитель: {company_name}
+Цена: {price}'''
+    await callback_query.message.edit_text(text, reply_markup=markup)
 
-bot.infinity_polling()
+
+@dp.callback_query_handler(lambda query: query.data.startswith('add_basket_'))
+async def add_basket(callback_query: types.CallbackQuery):
+    product_id = int(callback_query.data.split('_')[2])
+    db.add_product_to_basket(product_id, callback_query.message.chat.id)
+    await callback_query.message.edit_text(text='Продукт успешно добавлен в корзину', reply_markup=None)
+
+
+@dp.callback_query_handler(lambda query: query.data == 'show_product')
+async def show_basket(callback_query: types.CallbackQuery):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton(text='Оплатить корзину', callback_data='buy_basket'))
+    markup.add(types.InlineKeyboardButton(text='Очистить корзину', callback_data='clear_basket'))
+    markup.add(types.InlineKeyboardButton(text='Назад', callback_data='main_menu'))
+    basket_txt = db.create_basket_txt(callback_query.message.chat.id)
+    user = db.get_user(callback_query.message.chat.id)
+    balance = user[1]
+    basket = user[2]
+    cost = 0
+    for product_id in basket.split():
+        prodct = db.get_product_by_id(int(product_id))
+        price = prodct[3]
+        cost += price
+    text = f'''Ваш баланс: {balance}
+Стоимость корзины: {cost}
+Ваша корзина:
+'''
+
+    text += basket_txt
+    await callback_query.message.edit_text(text=text, reply_markup=markup)
+
+
+@dp.callback_query_handler(lambda query: query.data == 'clear_basket')
+async def clear_basket(callback_query: types.CallbackQuery):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton(text='Меню', callback_data='main_menu'))
+    db.clean_basket(callback_query.message.chat.id)
+    await callback_query.message.edit_text('Корзина очищена', reply_markup=markup)
+
+
+@dp.callback_query_handler(lambda query: query.data == 'buy_basket')
+async def buy_basket(callback_query: types.CallbackQuery):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton(text='Меню', callback_data='main_menu'))
+    user = db.get_user(callback_query.message.chat.id)
+    balance = user[1]
+    basket = user[2]
+    if not basket:
+        await callback_query.message.edit_text('Корзина пуста', reply_markup=markup)
+        return
+    cost = 0
+    for product_id in basket.split():
+        product = db.get_product_by_id(int(product_id))
+        price = product[3]
+        cost += price
+    if cost > balance:
+        await callback_query.message.edit_text('Недостатачно средств на балансе', reply_markup=markup)
+        return
+    await callback_query.message.edit_text('''Покупка прошла успешно.
+Ожидайте сообщение от администратора.''', reply_markup=markup)
+    user_link = await callback_query.message.chat.get_url()
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton(text='Написать покупателю', url=user_link))
+    text = f'''Пользователь {callback_query.message.chat.username} оплатил покупку
+В заказ входят:'''
+    basket_txt = db.create_basket_txt(callback_query.message.chat.id)
+    text += basket_txt
+    await bot.send_message(chat_id=config.admin_id, text=text)
+    db.spend_balance(callback_query.message.chat.id, cost)
+    db.clean_basket(callback_query.message.chat.id)
+
+
+if __name__ == '__main__':
+    db.start()
+    executor.start_polling(dp, skip_updates=True)
